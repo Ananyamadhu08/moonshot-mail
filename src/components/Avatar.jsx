d3401d32d6b48d779cc3c59ec8c986e1d290dc5e
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2023-01-24 22:12:55 +0530

    feat: basic UI skeleton

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,9 @@
+function Avatar({ name }) {
+    return (
+      <div className="avatar">
+        <p>{name[0]}</p>
+      </div>
+    );
+  }
+  
+  export default Avatar;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import { FilterBar } from "./FilterBar";
+import { EmailCard } from "./EmailCard";
+import { MainEmailSection } from "./MainEmailSection";
+
+export { FilterBar, EmailCard, MainEmailSection };
