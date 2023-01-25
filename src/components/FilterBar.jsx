5e508a870ec3baa6ca7686b9a4acb7d75f760b18
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2023-01-25 23:46:13 +0530

    feat: updated styles to modules

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,12 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import { updateFilterType } from "../features/slices/emailSlice";
+import styles from "./FilterBar.module.css";
+
 function FilterBar() {
   const options = ["Unread", "Read", "Favorites"];
+
+  const {
+    emails: { filterType },
+  } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
   return (
-    <div className="filter-bar">
-      <p>Filter By: </p>
+    <div className={styles.filterBarContainer}>
+      <p className={styles.text}>Filter By: </p>
       {options.map((option) => (
-        <div key={option} className="filter-bar-btn">
+        <button
+          onClick={() => dispatch(updateFilterType({ filterType: option }))}
+          key={option}
+          className={
+            option === filterType
+              ? styles.filterBarButtonActive
+              : styles.filterBarButton
+          }
+        >
           {option}
-        </div>
+        </button>
       ))}
     </div>
   );
